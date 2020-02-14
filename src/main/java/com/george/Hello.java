d5b6f05e1b2d80fbff0fc1167c9dc964917c0e64
george816 <chingyiliu@gmail.com>
2020-02-14 14:07:00 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/george/Hello.java
+++ src/main/java/com/george/Hello.java
@@ -6,9 +6,9 @@ public class Hello {
 //        new Person().hello();
         Person p = new Person();
         p.hello();
-        int age = 19; //基本資料型態，為全小寫，共有8種(short, int, long, float, double, char, string, boolean)
-        Integer age2 = 19;; //參照資料型態，為類別因此首字母大寫，每種基本資料型態都有對應的參照資料型態，又稱包裝類別(Wrapper Class)
-        char c = '錢';
+        int age = 19; //Primitive data types (short, int, long, float, double, char, string, boolean)
+        Integer age2 = 19;; //Reference data types, or named "Wrapper Class"
+        char c = 'A';
         Character c2 = c;
     }
 }
